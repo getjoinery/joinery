66bd0c5597bc762c5174a619f20add02f39c7ba8
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2021-02-03 10:44:04 -0500

    Move robots.txt to theme

---
+++ public_html/robots.php
@@ -0,0 +1,18 @@
+<?php
+echo 'User-agent: *\r\n";
+echo 'Disallow: /admin/\r\n";
+echo 'Disallow: /ajax/\r\n";
+echo 'Disallow: /analytics/\r\n";
+echo 'Disallow: /data/\r\n";
+echo 'Disallow: /logic/\r\n";
+echo 'Disallow: /includes/\r\n";
+echo 'Disallow: /page_scripts/\r\n";
+echo 'Disallow: /phpincludes/\r\n";
+echo 'Disallow: /profile/\r\n";
+echo 'Disallow: /template/\r\n";
+echo 'Disallow: /test/\r\n";
+echo 'Disallow: /theme/\r\n";
+echo 'Disallow: /utils/\r\n";
+echo 'Disallow: /wp-content/\r\n";
+echo 'Disallow: /uploads/\r\n";
+?>
