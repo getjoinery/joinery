6d7ff12c1f9547870e6cd778e544c9c74e607c14
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2021-03-17 10:31:34 -0400

    Debug email log

---
+++ public_html/adm/admin_debug_email_log.php
@@ -0,0 +1,40 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/Activation.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/ErrorHandler.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/FormWriterMaster.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/AdminPage-uikit3.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/SessionControl.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/DbConnector.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/debug_email_logs_class.php');
+
+
+	$session = SessionControl::get_instance();
+	$session->check_permission(8);
+	
+	$debug_email_log = new DebugEmailLog($_REQUEST['del_debug_email_log_id'], TRUE);
+	
+
+	$page = new AdminPage();
+	$page->admin_header(	
+	array(
+		'menu-id'=> 12,
+		'breadcrumbs' => array(
+			'DebugEmailLogs'=>'/admin/admin_debug_email_logs', 
+			$debug_email_log->get('del_subject') => '',
+		),
+		'session' => $session,
+	)
+	);		
+
+	$pageoptions['title'] = 'DebugEmailLog: '.$debug_email_log->get('del_subject');
+	$altlinks = array();
+	$pageoptions['altlinks'] = $altlinks;
+	$page->begin_box($pageoptions);
+	echo '<iframe src="/ajax/debug_email_log_preview_ajax?del_debug_email_log_id='.$debug_email_log->key.'" width="100%" height="300" style="border:1px solid gray;"></iframe>';			 
+	$page->end_box(); 
+
+
+	$page->admin_footer();
+
+?>
---
+++ public_html/adm/admin_debug_email_logs.php
@@ -0,0 +1,73 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/AdminPage-uikit3.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/FormWriterMaster.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/debug_email_logs_class.php');
+
+	$session = SessionControl::get_instance();
+	$session->check_permission(8);
+	//$session->set_return();
+
+	$numperpage = 30;
+	$offset = LibraryFunctions::fetch_variable('offset', 0, 0, '');
+	$sort = LibraryFunctions::fetch_variable('sort', 'debug_email_log_id', 0, '');
+	$sdirection = LibraryFunctions::fetch_variable('sdirection', 'DESC', 0, '');
+
+	$searchterm = LibraryFunctions::fetch_variable('searchterm', '', 0, '');
+
+	$page = new AdminPage();
+	$page->admin_header(	
+	array(
+		'menu-id'=> 12,
+		'page_title' => 'Users',
+		'readable_title' => 'Users',
+		'breadcrumbs' => array(
+			'DebugEmailLogs'=>'', 
+		),
+		'session' => $session,
+	)
+	);	
+	
+	
+	$search_criteria = array();
+	//$search_criteria['debug_email_log_like'] = $searchterm;
+
+	$debug_email_logs = new MultiDebugEmailLog(
+		$search_criteria,
+		array($sort=>$sdirection),
+		$numperpage,
+		$offset,
+		'OR');
+	$numrecords = $debug_email_logs->count_all();
+	$debug_email_logs->load();	
+
+		
+	$headers = array('Debug Email', 'Time');
+	$altlinks = array('Delete All' => '/admin/admin_debug_email_logs?action=delete_all');
+	$pager = new Pager(array('numrecords'=>$numrecords, 'numperpage'=> $numperpage));
+	$table_options = array(
+		//'sortoptions'=>array("User ID"=>"user_id", "Last Name"=>"last_name", "First Name"=>"first_name"),
+		'altlinks' => $altlinks,
+		'title' => 'Debug Email Logs',
+		//'search_on' => TRUE
+	);
+	$page->tableheader($headers, $table_options, $pager);
+
+
+	foreach($debug_email_logs as $debug_email_log) {
+		
+		$rowvalues = array();
+		array_push($rowvalues, '<a href="/admin/admin_debug_email_log?del_debug_email_log_id='.$debug_email_log->key.'">'.$debug_email_log->get('del_subject').'</a>');
+
+		$time = 'Sent: '. LibraryFunctions::convert_time($debug_email_log->get('del_create_time'), "UTC", $session->get_timezone());
+		array_push($rowvalues, $time);		
+	
+		$page->disprow($rowvalues);
+	}
+		
+	$page->endtable($pager);		
+
+	$page->admin_footer();
+
+?>
