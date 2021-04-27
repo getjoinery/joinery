97817a228857686fcfa45a9190de64c989cc63e8
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2021-04-27 19:44:20 -0400

    Fix subscriptions.

--- public_html/logic/orders_recurring_action_logic.php
+++ public_html/logic/orders_recurring_action_logic.php
@@ -2,6 +2,7 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/SessionControl.php');
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/address_class.php');
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/users_class.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/order_items_class.php');
 	
 	require_once($_SERVER['DOCUMENT_ROOT'].'/includes/stripe-php/init.php');
 
@@ -15,17 +16,28 @@
 	$session = SessionControl::get_instance();
 	$session->check_permission(0);
 	
+	$order_item_id = LibraryFunctions::fetch_variable('order_item_id', NULL,1,'order_item_id');
+	$order_item = new OrderItem($order_item_id, TRUE);	
+	$order_item->authenticate_write($session);
+	
 	$settings = Globalvars::get_instance();
 	\Stripe\Stripe::setApiKey($settings->get_setting('stripe_api_key'));	
 
-	if (!isset($_REQUEST['stripe_sid'])) {
-		throw new SystemInvalidFormError('The subscription id is invalid.');
-	}
 
 	//TODO SECURITY CHECK THE USER HERE
 
-	$sub = \Stripe\Subscription::retrieve($_REQUEST['stripe_sid']);
+	$sub = \Stripe\Subscription::retrieve($order_item->get('odi_stripe_subscription_id'));
 	$sub->cancel();
+	
+	$stripe_subscription = \Stripe\Subscription::retrieve($order_item->get('odi_stripe_subscription_id'));	
+	if($stripe_subscription[status] == 'canceled'){
+		$canceled_at = gmdate("c", $stripe_subscription[canceled_at]);
+		//IF SUBSCRIPTION ENDED, REMOVE 
+
+		$order_item->set('odi_subscription_cancelled_time', $canceled_at);
+		$order_item->save();
+
+	}
 
 	//NOW REDIRECT
 	$session = SessionControl::get_instance();
