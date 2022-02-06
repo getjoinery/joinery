1d88ae3b030115631c3a25f9043d775cb4a1d726
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-02-06 15:28:17 -0500

    Bug fix.

--- public_html/views/product.php
+++ public_html/views/product.php
@@ -107,7 +107,7 @@ $page->public_header(array(
       <!-- Image gallery -->
 	  
       <div class="mt-8 lg:mt-0 lg:col-start-1 lg:col-span-7 lg:row-start-1 lg:row-span-3">
-        <h2 class="sr-only">Images</h2>
+        <!--<h2 class="sr-only">Images</h2>
 
         <div class="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-3 lg:gap-8">
           <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg" alt="Back of women&#039;s Basic Tee in black." class="lg:col-span-2 lg:row-span-2 rounded-lg">
@@ -116,7 +116,7 @@ $page->public_header(array(
 
           <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg" alt="Front of women&#039;s Basic Tee in black." class="hidden lg:block rounded-lg">
         </div>
-      </div>
+      </div>-->
 
       <div class="mt-8 lg:col-span-5">
 	  <?php /* ?>
