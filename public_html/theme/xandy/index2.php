f8a19675638bd670174e0212ae07dc585d291e02
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-02-07 12:39:55 -0500

    Small change.

--- public_html/theme/xandy/index2.php
+++ public_html/theme/xandy/index2.php
@@ -40,9 +40,9 @@
               </p>
               <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                 <div class="space-y-4 sm:space-y-0 sm:mx-auto ">
-                  <a href="/events" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
+                  <!--<a href="/events" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
                     See Courses
-                  </a>
+                  </a>-->
                   <!--<a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8">
                     Live demo
                   </a>-->
