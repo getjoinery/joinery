9cc52e9138c83cf637cd9f6395551e53df252d09
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-02-08 21:24:16 -0500

    Update 404 page.

--- public_html/views/404.php
+++ public_html/views/404.php
@@ -1,25 +1,24 @@
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT'].'/includes/LibraryFunctions.php');
-	require_once(LibraryFunctions::get_theme_path().'/includes/PublicPage.php');
-	require_once(LibraryFunctions::get_theme_path().'/includes/FormWriterPublic.php');
+	require_once(LibraryFunctions::get_theme_path().'/includes/PublicPageTW.php');
+	require_once(LibraryFunctions::get_theme_path().'/includes/FormWriterPublicTW.php');
 
-	$page = new PublicPage();
+	$page = new PublicPageTW();
 	$hoptions = array(
 		'is_valid_page' => $is_valid_page,
 		'title' => 'Page not found', 
 		'is_404' => 1,
 	);
 	$page->public_header($hoptions);
-	echo PublicPage::BeginPage('Page not found');
-	echo '<div class="section padding-top-20">
-			<div class="container">';
+	echo PublicPageTW::BeginPage('Page not found');
+	echo PublicPageTW::BeginPanel();
 	?>
 
 	<h2>This page may have moved or is no longer available</h2>
 
 	<?php
-	echo '</div></div>';
-	echo PublicPage::EndPage();
+	echo PublicPageTW::EndPanel();
+	echo PublicPageTW::EndPage();
 
 	$page->public_footer(array('track'=>TRUE, 'is_404'=> 1));
 ?>
