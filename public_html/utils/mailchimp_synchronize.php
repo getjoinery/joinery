2755e8e851dc0bae53a33d110743098149b9bcef
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-03-22 15:33:13 -0400

    Improve mailchimp integration.

--- public_html/utils/mailchimp_synchronize.php
+++ public_html/utils/mailchimp_synchronize.php
@@ -1,49 +1,113 @@
 <?php
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/EmailTemplate.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/users_class.php');
 
 	$settings = Globalvars::get_instance();
 	$composer_dir = $settings->get_setting('composerAutoLoad');	
 	require $composer_dir.'autoload.php';
 	use MailchimpAPI\Mailchimp;
 
-	echo 'turned off';
-	exit();
 
 
 	$session = SessionControl::get_instance();
-	//$session->check_permission(0);
+	$session->check_permission(10);
 	$session_id = $_GET['session_id']; 
-	
-
-	
 	
 	$settings = Globalvars::get_instance();
 
-	for ($x=0; $x<=2700; $x+=50){
+
+	for ($x=0; $x<=10000; $x+=1000){
 		echo $x.'<br>';
 		$mailchimp = new Mailchimp($settings->get_setting('mailchimp_api_key'));
 		$return = $mailchimp
 		->lists($settings->get_setting('mailchimp_list_id'))
 		->members()
 		->get([
-			"count" => "50", 
+			"count" => "1000", 
 			"offset" => $x
 		]);
 		$results = $return->deserialize();
+		if(count($results->members) == 0){
+			echo 'DONE';
+			exit;
+		}
 		foreach ($results->members as $result){ 
 			$user = User::GetByEmail($result->email_address);
 			if($user){
-				if($result->status == 'subscribed'){
-					$user->set('usr_contact_preferences', 1);
-					$user->save();
+				echo $user->key. ': '.$result->last_changed . ' -- '. $user->get('usr_contact_preference_last_changed');
+				
+				if($result->status == 'subscribed' && $user->get('usr_contact_preferences') == 1){
+					//NO CHANGE	
+					echo ' S';
 				}
-				else{
-					$user->set('usr_contact_preferences', 0);
-					$user->save();			
+				else if($result->status == 'unsubscribed' && $user->get('usr_contact_preferences') == 0){
+					//NO CHANGE
+					echo ' U';
+				}
+				else if($result->status == 'subscribed' && $user->get('usr_contact_preferences') == 0){
+					if(!$user->get('usr_contact_preference_last_changed') || $user->get('usr_contact_preference_last_changed') < $result->last_changed){
+						//MAILCHIMP IS MOST RECENT.  UPDATE LOCALLY
+						echo ' subscribe locally ';
+						$user->set('usr_contact_preferences', 1);
+						$user->set('usr_contact_preference_last_changed', $result->last_changed);
+					}
+					else if ($user->get('usr_contact_preference_last_changed') >= $result->last_changed){
+						//LOCAL IS MOST RECENT.  UPDATE MAILCHIMP
+						$merge_values = [
+							"FNAME" => $user->get('usr_first_name'),
+							"LNAME" => $user->get('usr_last_name'),
+							"MMERGE3" => 'Yes',
+						];
+
+						$post_params = [
+							"status" => "unsubscribed", 
+							"merge_fields" => $merge_values,
+						];
+						$return = $mailchimp
+							->lists($settings->get_setting('mailchimp_list_id'))
+							->members(md5(strtolower($user->get('usr_email'))))
+							->put($post_params);
+						
+						$status = $return->deserialize();
+						echo ' set mailchimp to unsubscribed';
+					}
+				}
+				else if($result->status == 'unsubscribed' && $user->get('usr_contact_preferences') == 1){
+					if(!$user->get('usr_contact_preference_last_changed') || $user->get('usr_contact_preference_last_changed') < $result->last_changed){
+						//MAILCHIMP IS MOST RECENT.  UPDATE LOCALLY
+						$user->set('usr_contact_preferences', 0);
+						$user->set('usr_contact_preference_last_changed', $result->last_changed);
+						echo ' unsubscribe locally ';
+					}
+					else if($user->get('usr_contact_preference_last_changed') >= $result->last_changed){
+						//LOCAL IS MOST RECENT.  UPDATE MAILCHIMP
+						$merge_values = [
+							"FNAME" => $user->get('usr_first_name'),
+							"LNAME" => $user->get('usr_last_name'),
+							"MMERGE3" => 'Yes',
+						];
+
+						$post_params = [
+							"status" => "pending", 
+							"merge_fields" => $merge_values,
+						];
+						$return = $mailchimp
+							->lists($settings->get_setting('mailchimp_list_id'))
+							->members(md5(strtolower($user->get('usr_email'))))
+							->put($post_params);
+						
+						$status = $return->deserialize();
+						echo ' set mailchimp to unsubscribed';
+					}
+				}
+				echo '<br>';
+				if($user->get('usr_first_name') && $user->get('usr_last_name') && $user->get('usr_email')){
+					$user->save();
 				}
 			}
 		}
 	}
+	
 	exit();
 	?>
