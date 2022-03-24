b11ca5b60a3545dab6ad32797777f76017a29628
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-03-24 15:04:32 -0400

    Switch GT homepage.

---
+++ public_html/theme/galactictribune/index.php
@@ -0,0 +1,9 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/SessionControl.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
+	require_once(LibraryFunctions::get_theme_path().'/includes/PublicPage.php');
+	require_once(LibraryFunctions::get_theme_path().'/includes/FormWriterPublic.php');
+	require_once(LibraryFunctions::get_logic_file_path('blog_logic.php'));
+ 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/views/blog.php');
+?>
