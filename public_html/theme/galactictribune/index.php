2ffde56ca50cf963d321de2678a4bf1d12db5ccc
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-03-24 15:20:27 -0400

    Remove old publicpage.

--- public_html/theme/galactictribune/index.php
+++ public_html/theme/galactictribune/index.php
@@ -1,9 +1,4 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/SessionControl.php');
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
-	require_once(LibraryFunctions::get_theme_path().'/includes/PublicPage.php');
-	require_once(LibraryFunctions::get_theme_path().'/includes/FormWriterPublic.php');
-	require_once(LibraryFunctions::get_logic_file_path('blog_logic.php'));
  	
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/views/blog.php');
 ?>
