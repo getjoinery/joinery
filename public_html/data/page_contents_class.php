c8842a47d6e356f7bf6f2e3eba35a0d358ebc63e
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-04-22 09:07:05 -0400

    Update to page content choose.

--- public_html/data/page_contents_class.php
+++ public_html/data/page_contents_class.php
@@ -246,7 +246,7 @@ class MultiPageContent extends SystemMultiBase {
 	}
 
 	function load($debug = false) {
-		$q = $this->_get_results();
+		$q = $this->_get_results(false, $debug);
 		foreach($q->fetchAll() as $row) {
 			$child = new PageContent($row->pac_page_content_id);
 			$child->load_from_data($row, array_keys(PageContent::$fields));
@@ -255,7 +255,7 @@ class MultiPageContent extends SystemMultiBase {
 	}
 
 	function count_all($debug = false) {
-		$q = $this->_get_results(TRUE);
+		$q = $this->_get_results(TRUE, $debug);
 		$counter = $q->fetch();
 		return $counter->count;
 	}
