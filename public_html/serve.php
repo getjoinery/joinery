78d7a0b274b138b2bd9ac996148342e7dc437609
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-04-27 09:26:04 -0400

    Bug fix.

--- public_html/serve.php
+++ public_html/serve.php
@@ -246,6 +246,21 @@ if($settings->get_setting('blog_active')){
 	require_once($_SERVER['DOCUMENT_ROOT'].'/data/posts_class.php');
 	$blog_subdirectory = $settings->get_setting('blog_subdirectory');
 
+	if($params[0] == $blog_subdirectory && !$params[1]){
+		$template_file = $template_directory.'/blog.php';
+		$base_file = $_SERVER['DOCUMENT_ROOT'].'/views/blog.php';
+		
+		$is_valid_page = true; 
+
+		if(file_exists($template_file)){
+			require_once($template_file);
+			exit();
+		}
+		else if(file_exists($base_file)){
+			require_once($base_file); 
+			exit();		
+		}
+	}
 	if($params[0] == $blog_subdirectory){
 		$post = Post::get_by_link($params[1]);	
 	}
