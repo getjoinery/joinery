6a3fd2089c4601c067110fca6699121feb29af33
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-05-02 10:51:40 -0400

    Remove admin panel.

--- public_html/includes/AdminPage-uikit3.php
+++ public_html/includes/AdminPage-uikit3.php
@@ -158,62 +158,7 @@ class AdminPage{
 
 
 		<!--HEADER-->
-		<header id="top-head" class="uk-position-fixed">
-
-			
-	<?php
-	
-	if($_SESSION['permission'] == 10){
-		echo '		<style type="text/css">
-table.example3 {background-color:white;border-collapse:collapse;width:100%;display:block;}
-table.example3 th, table.example3 td {text-align:center;border:1px solid black;padding:5px;}
-table.example3 th {background-color:AntiqueWhite;}
-table.example3 td:first-child {width:20%;}
-</style>';
-		echo '<div id="admin_panel" style="display:none;">';
-		echo '<table class = "example3"><th colspan=2>Session</th>';
-		foreach($_SESSION as $sname=>$svar){
-			echo '<tr><td>'.$sname . '</td><td>';
-			if(is_array($svar)){
-				print_r($svar);
-			}
-			else if(is_object($svar)){
-				var_dump($svar);
-			}
-			else{
-				echo $svar;
-			}
-			echo '</td></tr>';
-		}
-		echo '</table><br /><table class = "example3"><th colspan=2>Request</th>';
-		foreach($_REQUEST as $sname=>$svar){
-			echo '<tr><td>'.$sname . '</td><td>';
-			if(is_array($svar)){
-				print_r($svar);
-			}
-			else if(is_object($svar)){
-				var_dump($svar);
-			}
-			else{
-				echo $svar;
-			}
-			echo '</td></tr>';
-		}		
-		
-		echo '</table></div>';
-		//echo '<a id="admintoggle" href="#">toggle</a>';
-		echo '<script>
- 
-$(document).ready(function() {
-
-$("#admintoggle").click(function () {
-$("#admin_panel").toggle();
-});
-});
-</script>';
-	}
-	
-	?>			
+		<header id="top-head" class="uk-position-fixed">		
 			
 			
 			<div class="uk-container uk-container-expand uk-background-primary">		
