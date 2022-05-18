96100db13295dd8a1d0432aee7cb592eb6261bc1
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-05-18 12:55:48 -0400

    Amanda site v1

--- public_html/theme/empoweredmindtn/index.php
+++ public_html/theme/empoweredmindtn/index.php
@@ -20,6 +20,7 @@
 	?>
 	
      <!-- Hero card -->
+	  <!--
       <div class="relative">
         <div class="absolute inset-x-0 bottom-0 h-1/2"></div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -41,31 +42,31 @@
                   <a href="/events" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
                     See Courses
                   </a>
-                  <!--<a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8">
+                  <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8">
                     Live demo
-                  </a>-->
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>	
+	-->
 	
-	
 
 
-
+<!--
 <div class="py-12 bg-white">
   <div class="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
     <h2 class="sr-only">Recorded Classes</h2>
     <dl class="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8">
       <div>
         <dt>
-          <!--<div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
             </svg>
-          </div>-->
+          </div>
           <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Recorded Classes</p>
         </dt>
         <dd class="mt-2 text-base text-gray-500">
@@ -75,11 +76,11 @@
 
       <div>
         <dt>
-          <!--<div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
             </svg>
-          </div>-->
+          </div>
           <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Specificity without Dogma ✨</p>
         </dt>
         <dd class="mt-2 text-base text-gray-500">
@@ -89,11 +90,11 @@
 
       <div>
         <dt>
-          <!--<div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
             </svg>
-          </div>-->
+          </div>
           <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Hilarious Delivery 🤣</p>
         </dt>
         <dd class="mt-2 text-base text-gray-500">
@@ -103,7 +104,7 @@
     </dl>
   </div>
 </div>
-
+-->
 
 
 
@@ -189,7 +190,7 @@
       </div>
       <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
         <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="https://devonandjerry.com/uploads/medium/Decon_zouk_5_x74ki5j.jpg" alt="Customer profile user interface">
+          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="/uploads/medium/AmandaMartinheadshot_wkhgv4r.jpg" alt="Amanda Martin">
         </div>
       </div>
     </div>
