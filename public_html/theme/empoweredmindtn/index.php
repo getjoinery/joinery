f00d13800a711e29f27db21af2e1462153404fba
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-05-19 09:02:49 -0400

    Amanda site update.

--- public_html/theme/empoweredmindtn/index.php
+++ public_html/theme/empoweredmindtn/index.php
@@ -55,57 +55,6 @@
 	
 
 
-<!--
-<div class="py-12 bg-white">
-  <div class="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="sr-only">Recorded Classes</h2>
-    <dl class="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8">
-      <div>
-        <dt>
-          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-            </svg>
-          </div>
-          <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Recorded Classes</p>
-        </dt>
-        <dd class="mt-2 text-base text-gray-500">
-          Find it challenging to make it onto live zoom sessions or live in a dance wasteland? We've got you covered. All our courses are hi-def, succinctly edited, and vouched for by many!
-        </dd>
-      </div>
-
-      <div>
-        <dt>
-          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-            </svg>
-          </div>
-          <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Specificity without Dogma ✨</p>
-        </dt>
-        <dd class="mt-2 text-base text-gray-500">
-          Frustrated with vague descriptions of "energy"? We like to get specific, detached of dogma, with vibrant imagery that shows you all possibilities informed by anatomy and physics.
-        </dd>
-      </div>
-
-      <div>
-        <dt>
-          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-            </svg>
-          </div>
-          <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Hilarious Delivery 🤣</p>
-        </dt>
-        <dd class="mt-2 text-base text-gray-500">
-          We're pretty funny - just ask our therapists and our students.
-        </dd>
-      </div>
-    </dl>
-  </div>
-</div>
--->
-
 
 
 <div class="relative bg-white pt-16 pb-32 overflow-hidden">
@@ -178,10 +127,7 @@
               Hello! I’m Amanda Martin. I’ve been
 teaching since 2008 and have been
 working with kids for over 20 years. My
-passion for the development of the
-whole child (mind, heart, & body)
-compels my work with kids in and out of
-the classroom.
+passion is whole child development through the lens of social emotional learning.
             </h2>
             <!--<p class="mt-4 text-lg text-gray-500">
               Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.
@@ -204,6 +150,55 @@ the classroom.
 </div>
 
 
+
+<div class="py-12 bg-white">
+  <div class="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 class="sr-only">Benefits of Social Emotional Learning</h2>
+    <dl class="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8">
+      <div>
+        <dt>
+          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+            </svg>
+          </div>
+          <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Healthy Relationships</p>
+        </dt>
+        <dd class="mt-2 text-base text-gray-500">
+          Social Emotional Learning (SEL) results in the development and maintenance of healthy relationships
+        </dd>
+      </div>
+
+      <div>
+        <dt>
+          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+            </svg>
+          </div>
+          <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Effective Communication</p>
+        </dt>
+        <dd class="mt-2 text-base text-gray-500">
+          SEL exercises teach effective communication strategies to use during conflict with family and peers
+        </dd>
+      </div>
+
+      <div>
+        <dt>
+          <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+            </svg>
+          </div>
+          <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Learn Boundaries</p>
+        </dt>
+        <dd class="mt-2 text-base text-gray-500">
+          SEL exercises help kids understand and apply boundaries within daily living 
+        </dd>
+      </div>
+    </dl>
+  </div>
+</div>
 
 
 	
