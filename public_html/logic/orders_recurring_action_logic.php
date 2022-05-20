6d1f271458f650f9e5048532f158a2b60e5491df
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-05-20 08:52:10 -0400

    Bug fix on subscriptions.

--- public_html/logic/orders_recurring_action_logic.php
+++ public_html/logic/orders_recurring_action_logic.php
@@ -27,39 +27,53 @@
 	\Stripe\Stripe::setApiKey($settings->get_setting('stripe_api_key'));	
 
 	$sub = \Stripe\Subscription::retrieve($order_item->get('odi_stripe_subscription_id'));
-	$sub->cancel();
+	try {
+		$sub->cancel();
+	}					
+	catch (Exception $e) {
+		//DO NOTHING
+		$error = "We were unable to cancel that subscription.  Please contact the webmaster.";
+	}	
 	
-	$stripe_subscription = \Stripe\Subscription::retrieve($order_item->get('odi_stripe_subscription_id'));	
-	if($stripe_subscription[status] == 'canceled'){
-		$canceled_at = gmdate("c", $stripe_subscription[canceled_at]);
-		//IF SUBSCRIPTION ENDED, REMOVE 
-
-		$order_item->set('odi_subscription_cancelled_time', $canceled_at);
-		$order_item->save();
-		
-		//SEND NOTIFICATION
-		if($settings->get_setting('subscription_notification_emails')){
-			$notify_emails = explode(',', $settings->get_setting('subscription_notification_emails'));
-			foreach($notify_emails as $notify_email){
-				try {
-					$notify_user = User::GetByEmail($notify_email);
-					$body = 'Subscription '.$order_item->get('odi_stripe_subscription_id').' (Order '. $order->key .') was cancelled for user '.$order_user->display_name().' ('.$order_user->get('usr_email').')';
-					$email_inner_template = $settings->get_setting('individual_email_inner_template');
-					$email = new EmailTemplate($email_inner_template, $notify_user);
-					$email->fill_template(array(
-						'subject' => 'Cancelled Subscription',
-						'body' => $body,
-					));	
-					$result = $email->send();
-				}					
-				catch (Exception $e) {
-					//DO NOTHING
-					$error = "";
+	if(!$order_item->get('odi_stripe_subscription_id')){
+		$stripe_subscription = \Stripe\Subscription::retrieve($order_item->get('odi_stripe_subscription_id'));	
+		if($stripe_subscription[status] == 'canceled'){
+			$canceled_at = gmdate("c", $stripe_subscription[canceled_at]);
+			//IF SUBSCRIPTION ENDED, REMOVE 
+			
+			
+			$order_item->set('odi_subscription_cancelled_time', $canceled_at);
+			$order_item->save();
+			
+			//SEND NOTIFICATION
+			if($settings->get_setting('subscription_notification_emails')){
+				$notify_emails = explode(',', $settings->get_setting('subscription_notification_emails'));
+				foreach($notify_emails as $notify_email){
+					try {
+						$notify_user = User::GetByEmail($notify_email);
+						$body = 'Subscription '.$order_item->get('odi_stripe_subscription_id').' (Order '. $order->key .') was cancelled for user '.$order_user->display_name().' ('.$order_user->get('usr_email').')';
+						$email_inner_template = $settings->get_setting('individual_email_inner_template');
+						$email = new EmailTemplate($email_inner_template, $notify_user);
+						$email->fill_template(array(
+							'subject' => 'Cancelled Subscription',
+							'body' => $body,
+						));	
+						$result = $email->send();
+					}					
+					catch (Exception $e) {
+						//DO NOTHING
+						$error2 = "";
+					}
 				}
 			}
-		}
 
+		}
+		if($error){
+			echo $error;
+			exit;
+		}
 	}
+
 
 	//NOW REDIRECT
 	$session = SessionControl::get_instance();
