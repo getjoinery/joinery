5bd1b7f376f27b9f0624a556c6926c13a47a9884
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2022-06-26 15:22:00 -0400

    Copy update.

--- public_html/ajax/blog_post_preview_ajax.php
+++ public_html/ajax/blog_post_preview_ajax.php
@@ -10,7 +10,9 @@
 	$session->check_permission(5);
 
 	$post = new Post($_GET['pst_post_id'], TRUE);
-
+	//GET AUTHOR
+	$author = new User($post->get('pst_usr_user_id'), TRUE);
+	$tags = $post->get_tags();
 
 	$page = new PublicPageTW();
 	$hoptions = array(
@@ -24,9 +26,29 @@
 	
 
     ?>
+    <div class="text-lg max-w-prose mx-auto">
+      <h1>
+        <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Blog</span>
+        <span class="mt-2 mb-4 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"><?php echo $post->get('pst_title'); ?></span>
+      </h1>
+				<p class="text-base text-gray-500 text-center">
+					<?php echo $author->display_name().' at '; ?>
+				  <time datetime="2020-03-16"><?php echo LibraryFunctions::convert_time($post->get('pst_published_time'), 'UTC', 'America/New_York'); ?></time>
+				</p>
+	<div class="flow-root text-center">
+		<?php
+		foreach ($tags as $tag){
+			echo '<a href="/blog/tag/'.urlencode($tag).'" class="inline-block p-1">
+			<span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">'.$tag.'</span>
+			</a>';			
+		}
+		?>				   
+	</div> 
+    
     <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
       <?php echo $post->get('pst_body'); ?>
     </div>
+	</div>
 	<?php
 			
 	echo PublicPageTW::EndPanel();
