ead0d564c6dbc0411a5a0478df8f527b002946b9
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-03-05 09:11:40 -0500

    Update trumbowyg to 2.26

--- public_html/includes/FormWriterMaster.php
+++ public_html/includes/FormWriterMaster.php
@@ -455,11 +455,11 @@ class FormWriterMaster {
 		if($htmlmode == 'yes'){
 			$output .= '
 			
-			<script src="/adm/includes/Trumbowyg-master/dist/trumbowyg.min.js"></script>
-			<link rel="stylesheet" href="/adm/includes/Trumbowyg-master/dist/ui/trumbowyg.min.css">
-			<script src="/adm/includes/Trumbowyg-master/dist/plugins/cleanpaste/trumbowyg.cleanpaste.min.js"></script>
-			<script src="/adm/includes/Trumbowyg-master/dist/plugins/preformatted/trumbowyg.preformatted.min.js"></script>
-			<script src="/adm/includes/Trumbowyg-master/dist/plugins/allowtagsfrompaste/trumbowyg.allowtagsfrompaste.min.js"></script>
+			<script src="/adm/includes/Trumbowyg-2-26/dist/trumbowyg.min.js"></script>
+			<link rel="stylesheet" href="/adm/includes/Trumbowyg-2-26/dist/ui/trumbowyg.min.css">
+			<script src="/adm/includes/Trumbowyg-2-26/dist/plugins/cleanpaste/trumbowyg.cleanpaste.min.js"></script>
+			<script src="/adm/includes/Trumbowyg-2-26/dist/plugins/preformatted/trumbowyg.preformatted.min.js"></script>
+			<script src="/adm/includes/Trumbowyg-2-26/dist/plugins/allowtagsfrompaste/trumbowyg.allowtagsfrompaste.min.js"></script>
 			<script type="text/javascript">';
 			$output .= "
 				$(document).ready(function() {
