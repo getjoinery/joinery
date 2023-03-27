7f9e5b11de1fcd804303bf04d3fea3eac2a05f91
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-03-27 13:35:56 -0400

    Improved migrations.

---
+++ public_html/migrations/test_migration.php
@@ -0,0 +1,5 @@
+    <?php
+function test_migration(){
+	echo 'Running migration test_migration.'. "<br>\n";
+}
+?>
--- public_html/utils/class_list.php
+++ public_html/utils/class_list.php
@@ -120,6 +120,84 @@
 			'MailingListRegistrant'
 		);			
 
+
+		//DATABASE MIGRATIONS
+		//NOTE!!  ALL MIGRATIONS HAVE TO BE WRITTEN SUCH THAT THEY CAN BE RUN REPEATEDLY
+		//IT BAILS ON ERROR AND STOPS MIGRATIONS, IN CASE SOME LATER ONES ARE DEPENDENT ON EARLIER ONES
+		//IF THERE IS A TEST SQL AND IF IT RETURNS == 0, THEN WE RUN THE MIGRATION
+		//IF THERE IS NO TEST SQL, IT IS ASSUMED THAT WE ALWAYS RUN THE MIGRATION
+		//ALSO UPDATES LAST SYSTEM VERSION
+		$migrations = array();
+		$migrations[0][system_version] = '0.5';
+		$migrations[0][test] = "SELECT count(1) as count FROM amu_admin_menus WHERE amu_defaultpage = 'admin_product_requirements'";
+		$migrations[0][migration_sql] = 'INSERT INTO "public"."amu_admin_menus"("amu_menudisplay", "amu_parent_menu_id", "amu_defaultpage", "amu_order", "amu_min_permission", "amu_disable", "amu_icon") VALUES (\'Product Requirements\', 5, \'admin_product_requirements\', 5, 8, 0, \'\');';
+		
+		$migrations[1][system_version] = '0.5';
+		$migrations[1][test] = "SELECT count(1) as count FROM stg_settings WHERE stg_name = 'system_version'";
+		$migrations[1][migration_sql] = 'INSERT INTO "public"."stg_settings"("stg_name", "stg_value", "stg_usr_user_id", "stg_create_time", "stg_update_time", "stg_group_name") VALUES (\'system_version\', \'0.5\', 1, \'now()\', \'now()\', \'general\');';
+		
+		$migrations[2][system_version] = '0.5';
+		$migrations[2][test] = "SELECT count(1) as count FROM stg_settings WHERE stg_name = 'db_migration_version'";
+		$migrations[2][migration_sql] = 'INSERT INTO "public"."stg_settings"("stg_name", "stg_value", "stg_usr_user_id", "stg_create_time", "stg_update_time", "stg_group_name") VALUES (\'db_migration_version\', \'1\', 1, \'now()\', \'now()\', \'general\');';
+
+		$migrations[3][system_version] = '0.5.1';
+		$migrations[3][test] = NULL;
+		$migrations[3][migration_sql] = NULL;		
+
+		$migrations[4][system_version] = '0.5.2';
+		$migrations[4][test] = NULL;
+		$migrations[4][migration_sql] = NULL;				
+
+		$migrations[5][system_version] = '0.5.2';
+		$migrations[5][test] = NULL;
+		$migrations[5][migration_sql] = NULL;			
 	
+		$migrations[6][system_version] = '0.5.3';
+		$migrations[6][test] = "SELECT count(1) as count FROM amu_admin_menus WHERE amu_defaultpage = 'admin_mailing_lists'";
+		$migrations[6][migration_sql] = 'INSERT INTO "public"."amu_admin_menus"("amu_menudisplay", "amu_parent_menu_id", "amu_defaultpage", "amu_order", "amu_min_permission", "amu_disable", "amu_icon") VALUES (\'Mailing Lists\', 11, \'admin_mailing_lists\', 7, 8, 0, \'\');';		
+		
+		$migrations[7][system_version] = '0.5.4';
+		$migrations[7][test] = NULL;
+		$migrations[7][migration_sql] = 'UPDATE emt_email_templates SET emt_body=\'<br/>----
+<br/>
+<small>
+	{mailing_list_id}This email was sent to {~recipient}you{end}{recipient}*recipient->usr_email*{end} because you are subscribed to the list <i>*mailing_list_string*</i>.  Please <a href="*web_dir*/profile/mailing_lists_preferences?mailing_list_id=*mailing_list_id*&user=*recipient->key*&hash=*recipient->usr_authhash*&zone=ocu&*email_vars*">click here</a> to stop receiving <i>*mailing_list_string*</i> emails.  {end mailing_list_id}
+
+{~mailing_list_id}This email was sent to {~recipient}you{end}{recipient}*recipient->usr_email*{end} because you are registered on our site.  Please <a href="*web_dir*/profile">click here</a> to manage your preferences.  {end ~mailing_list_id}
+</small>
+<br/>
+<br/>\' WHERE emt_name=\'default_footer\';';				
+		
+		$migrations[8][system_version] = '0.5.4';
+		$migrations[8][test] = "SELECT count(1) as count FROM amu_admin_menus WHERE amu_defaultpage = 'admin_analytics_email_stats'";
+		$migrations[8][migration_sql] = 'INSERT INTO "public"."amu_admin_menus"("amu_menudisplay", "amu_parent_menu_id", "amu_defaultpage", "amu_order", "amu_min_permission", "amu_disable", "amu_icon") VALUES (\'Email Statistics\', 12, \'admin_analytics_email_stats\', 2, 5, 0, \'\');';	
+
+		$migrations[9][system_version] = '0.5.5';
+		$migrations[9][test] = "SELECT count(1) as count FROM stg_settings WHERE stg_name = 'mailing_lists_active'";
+		$migrations[9][migration_sql] = 'INSERT INTO "public"."stg_settings"("stg_name", "stg_value", "stg_usr_user_id", "stg_create_time", "stg_update_time", "stg_group_name") VALUES (\'mailing_lists_active\', 1, 1, \'now()\', \'now()\', \'general\');';		
+
+		$migrations[10][system_version] = '0.5.6';
+		$migrations[10][test] = NULL;
+		$migrations[10][migration_sql] = 'UPDATE "public"."emt_email_templates" SET "emt_body" = \'<br/>----
+<br/>
+<small>
+{mailing_list_id}This email was sent to {~recipient}you{end}{recipient}*recipient->usr_email*{end} because you are subscribed to the list <i>*mailing_list_string*</i>.  Please <a href="*web_dir*/profile/mailing_lists_preferences?mailing_list_id=*mailing_list_id*&user=*recipient->key*&hash=*recipient->usr_authhash*&zone=ocu&*email_vars*">click here</a> to stop receiving <i>*mailing_list_string*</i> emails.  {end mailing_list_id}
+
+{evr_event_registrant_id}This email was sent to {~recipient}you{end}{recipient}*recipient->usr_email*{end} because you are registered for an event or course.  To unsubscribe, you must withdraw from the event or course here:  <a href="*web_dir*/profile/event_withdraw?evr_event_registrant_id=*evr_event_registrant_id*&user=*recipient->key*&*email_vars*">unsubscribe</a>.  
+{end evr_event_registrant_id}
+
+{~mailing_list_id}
+This email was sent to {~recipient}you{end}{recipient}*recipient->usr_email*{end} because you are registered on our site.  Please <a href="*web_dir*/profile">click here</a> to manage your preferences. 
+{end ~mailing_list_id}
+</small>
+<br/>
+<br/>\', "emt_create_time" = \'2020-12-19 20:03:41.97081\', "emt_update_time" = \'2020-12-19 20:03:41.97081\', "emt_delete_time" = NULL WHERE emt_name=\'default_footer\';
+';
+		$migrations[10][migration_file] = NULL;	
+		
+		$migrations[10][system_version] = '0.5.7';
+		$migrations[10][test] = NULL;
+		$migrations[10][migration_sql] = NULL;
+		$migrations[10][migration_file] = 'test_migration.php';	
 ?>
 
