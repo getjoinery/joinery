251820ab497c62a141bcee3c3dbeba63dca4d7c9
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-04-07 10:14:14 -0400

    Bug fixes.

--- public_html/views/cart_charge.php
+++ public_html/views/cart_charge.php
@@ -2,7 +2,7 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
 	require_once (LibraryFunctions::get_logic_file_path('cart_charge_logic.php'));
 
-	$page_vars = cart_charge($_GET, $_POST);
+	$page_vars = cart_charge_logic($_GET, $_POST);
 	
 	//NOW REDIRECT TO CONFIRMATION PAGE
 	LibraryFunctions::Redirect('/cart_confirm'); 
--- public_html/views/cart_clear.php
+++ public_html/views/cart_clear.php
@@ -2,7 +2,7 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
 	require_once (LibraryFunctions::get_logic_file_path('cart_clear_logic.php'));
 
-	$page_vars = cart_clear($_GET, $_POST);
+	$page_vars = cart_clear_logic($_GET, $_POST);
 	
 	//NOW REDIRECT TO CONFIRMATION PAGE
 	LibraryFunctions::Redirect('/cart');
