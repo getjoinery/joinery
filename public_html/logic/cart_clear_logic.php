163f87c0f187a1c398571c28bca738ccfef75228
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-04-07 10:10:05 -0400

    Cart templating.

--- public_html/logic/cart_clear_logic.php
+++ public_html/logic/cart_clear_logic.php
@@ -1,18 +1,25 @@
 <?php
+
+function cart_clear($get_vars, $post_vars){
 	require_once($_SERVER['DOCUMENT_ROOT'].'/includes/ShoppingCart.php');
+	
+	$page_vars = array();
+
+	$session = SessionControl::get_instance();
+	$page_vars['session'] = $session;
 
 	$settings = Globalvars::get_instance();
+	$page_vars['settings'] = $settings;
 	if(!$settings->get_setting('products_active')){
 		header("HTTP/1.0 404 Not Found");
 		echo 'This feature is turned off';
 		exit();
 	}
 	
-	$session = SessionControl::get_instance();
-	
 	$cart = $session->get_shopping_cart();
-
 	$cart->clear_cart();
+	
+	return $page_vars;
+}
 
-	LibraryFunctions::Redirect('/cart');
 ?>
--- public_html/views/cart_charge.php
+++ public_html/views/cart_charge.php
@@ -1,4 +1,9 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
-require_once (LibraryFunctions::get_logic_file_path('cart_charge_logic.php'));
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
+	require_once (LibraryFunctions::get_logic_file_path('cart_charge_logic.php'));
+
+	$page_vars = cart_charge($_GET, $_POST);
+	
+	//NOW REDIRECT TO CONFIRMATION PAGE
+	LibraryFunctions::Redirect('/cart_confirm'); 
 ?>
