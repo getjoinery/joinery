f18046e86fc05d1e4c32c819e1c285c211ae9ff3
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-04-08 09:29:21 -0400

    Copy change.

--- public_html/includes/AdminPage-uikit3.php
+++ public_html/includes/AdminPage-uikit3.php
@@ -436,7 +436,7 @@ class AdminPage{
 						</div>
 				<footer class="uk-section uk-section-small uk-text-center">
 					<hr>
-					<p class="uk-text-small uk-text-center">Copyright 2020 - Jeremy Tunnell | Built with <a href="http://getuikit.com" title="Visit UIkit 3 site" target="_blank" data-uk-tooltip><span data-uk-icon="uikit"></span></a> </p>
+					<p class="uk-text-small uk-text-center">Copyright 2023 - Jeremy Tunnell | Built with <a href="http://getuikit.com" title="Visit UIkit 3 site" target="_blank" data-uk-tooltip><span data-uk-icon="uikit"></span></a> </p>
 				</footer>
 			</div>
 		
