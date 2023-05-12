9052981a734169b67795749bbed9a4525f5ccb0d
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-05-11 20:26:23 -0400

    Bug fix.

--- public_html/views/profile/event_sessions.php
+++ public_html/views/profile/event_sessions.php
@@ -7,7 +7,7 @@
 	$page_vars = event_sessions_logic($_GET, $_POST);
 
 	if($page_vars['error_message']){
-		PublicPageTW::OutputGenericPublicPage('Not Registered', 'Not Registered', $error_message);
+		PublicPageTW::OutputGenericPublicPage('Not Registered', 'Not Registered', $page_vars['error_message']);
 		exit();
 	}	
 
