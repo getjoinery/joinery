3a5d30445361766bc035749d7d4d6dd615660618
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-07-07 20:33:58 -0400

    Bug fix.

--- public_html/views/event.php
+++ public_html/views/event.php
@@ -175,8 +175,8 @@
           <section aria-labelledby="quick-links-title">
             <div class="rounded-lg bg-white overflow-hidden shadow p-6">
 			<h2 class="text-base font-medium text-gray-900" id="description-title">Location: <?php echo $page_vars['location_object']->get('loc_name'); ?></h2>
-			<?php if($page_vars['location_object']->get('loc_address')){ echo 'Address:  '. $page_vars['location']->get('loc_address'). '<br>'; } ?>
-			<?php if($page_vars['location_object']->get('loc_website')){ echo 'Website:  <a href="'. $page_vars['location']->get('loc_website'). '">'.$page_vars['location']->get('loc_website').'</a><br>'; } ?>
+			<?php if($page_vars['location_object']->get('loc_address')){ echo 'Address:  '. $page_vars['location_object']->get('loc_address'). '<br>'; } ?>
+			<?php if($page_vars['location_object']->get('loc_website')){ echo 'Website:  <a href="'. $page_vars['location_object']->get('loc_website'). '">'.$page_vars['location_object']->get('loc_website').'</a><br>'; } ?>
               
 				
 				<?php if($page_vars['location_picture']){ ?>
