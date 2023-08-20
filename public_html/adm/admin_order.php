7cf39d0388ca3c91fedfcbb0bd193f62ee8609ee
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-08-20 11:05:37 -0400

    Bug fix.

--- public_html/adm/admin_order.php
+++ public_html/adm/admin_order.php
@@ -121,7 +121,11 @@
 		}
 		
 		if($order_item->get('odi_subscription_cancelled_time')){
-			$this_out .= $order_item->get('odi_subscription_status'). ' at '.LibraryFunctions::convert_time($order_item->get('odi_subscription_cancelled_time'), 'UTC', $session->get_timezone());	
+			$status_words = 'canceled';
+			if($order_item->get('odi_subscription_status')){
+				$status_words = $order_item->get('odi_subscription_status');
+			}
+			$this_out .= ' '. $status_words. ' at '.LibraryFunctions::convert_time($order_item->get('odi_subscription_cancelled_time'), 'UTC', $session->get_timezone());	
 		}
 		else if($order_item->get('odi_subscription_status')){
 			$this_out .=  ' STATUS: '. $order_item->get('odi_subscription_status');
