a348ee9543f6064cb56445b8ee56bf0190fbc5ac
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-09-24 11:44:13 -0400

    More api updates.

--- public_html/utils/api_test.php
+++ public_html/utils/api_test.php
@@ -7,14 +7,14 @@
 		$access_token = '';	
 		$curl=curl_init();
 		curl_setopt_array($curl, array(
-		  CURLOPT_URL => 'https://jeremytunnell.net/api/v1/user/100000000?usr_permission=9',
+		  CURLOPT_URL => 'https://jeremytunnell.net/api/v1/user/41',
 		  CURLOPT_RETURNTRANSFER => true,
 		  CURLOPT_ENCODING => '',
 		  CURLOPT_MAXREDIRS => 10,
 		  CURLOPT_TIMEOUT => 0,
 		  CURLOPT_FOLLOWLOCATION => true,
 		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => 'GET',
+		  CURLOPT_CUSTOMREQUEST => 'DELETE',
 		  CURLOPT_HTTPHEADER => array(
 			"public_key: ".$public_key,
 			"secret_key: ".$secret_key,
