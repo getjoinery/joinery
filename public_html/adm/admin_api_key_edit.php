8e963a0c75c55002458949574938f35991c1a3c6
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-09-24 20:56:45 -0400

    Full api key checkin.

--- public_html/adm/admin_api_key_edit.php
+++ public_html/adm/admin_api_key_edit.php
@@ -27,6 +27,20 @@
 			$api_key->set($field, $_POST[$field]);
 		}
 
+		if($_POST['apk_start_time_date'] && $_POST['apk_start_time_time']){
+			$time_combined = $_POST['apk_start_time_date'] . ' ' . LibraryFunctions::toDBTime($_POST['apk_start_time_time']);
+			$utc_time = LibraryFunctions::convert_time($time_combined, $session->get_timezone(),  'UTC', 'c');
+			$api_key->set('apk_start_time', $utc_time);
+			//$api_key->set('apk_start_time_local', $time_combined);
+		}
+		
+		if($_POST['apk_expires_time_date'] && $_POST['apk_expires_time_time']){
+			$time_combined = $_POST['evt_expires_time_date'] . ' ' . LibraryFunctions::toDBTime($_POST['evt_expires_time_time']);
+			$utc_time = LibraryFunctions::convert_time($time_combined, $session->get_timezone(),  'UTC', 'c');
+			$api_key->set('evt_expires_time', $utc_time);	
+			//$api_key->set('evt_expires_time_local', $time_combined);			
+		}
+		
 		$api_key->set('apk_usr_user_id', $session->get_user_id());
 		$public_key = 'public_'.LibraryFunctions::random_string(16);
 		//$secret_key = 'secret_'.LibraryFunctions::random_string(16);
@@ -42,14 +56,13 @@
 		exit;
 	}
 
-	$title = $api_key->get('apk_name');
-	$content = $api_key->get('apk_description');
+
 	
 	
 
 
-	$api_keyt = new AdminPage();
-	$api_keyt->admin_header(	
+	$page = new AdminPage();
+	$page->admin_header(	
 	array(
 		'menu-id'=> 'api_keys',
 		'breadcrumbs' => array(
@@ -61,8 +74,8 @@
 	);	
 
 	
-	$api_keyoptions['title'] = "Edit ApiKey";
-	$api_keyt->begin_box($api_keyoptions);
+	$pageoptions['title'] = "Edit ApiKey";
+	$page->begin_box($pageoptions);
 
 	
 	// Editing an existing email
@@ -82,19 +95,20 @@
 	}
 	
 	echo $formwriter->textinput('Key name', 'apk_name', NULL, 100, $api_key->get('apk_name'), '', 255, '');		
-	
-	//echo $formwriter->textinput('Public street address', 'apk_address', NULL, 100, $api_key->get('apk_address'), '', 255, '');
-	//echo $formwriter->textinput('ApiKey website', 'apk_website', NULL, 100, $api_key->get('apk_website'), '', 255, '');
 
-	$optionvals = array("No"=>0, "Yes"=>1);
+	$optionvals = array("Yes"=>1,"No"=>0);
 	echo $formwriter->dropinput("Active", "apk_is_active", "", $optionvals, $api_key->get('apk_is_active'), '', FALSE);
 	
 	$optionvals = array("Read only"=>1, "Write only"=>2, "Read/Write"=>3, "Read/Write/Delete"=>4);
 	echo $formwriter->dropinput("Permission", "apk_permission", "", $optionvals, $api_key->get('apk_permission'), '', FALSE);
-	
-	
-	echo $formwriter->textinput('Allowed IP addresses (comma separated)', 'apk_ip_restriction', NULL, 100, $api_key->get('apk_ip_restriction'), '', 255, '');
+		
+	echo $formwriter->textinput('Allowed IP addresses (comma separated) (optional)', 'apk_ip_restriction', NULL, 100, $api_key->get('apk_ip_restriction'), '', 255, '');
 
+	echo $formwriter->datetimeinput('Key start time (optional)', 'apk_start_time', 'ctrlHolder', LibraryFunctions::convert_time($api_key->get('apk_start_time'), 'UTC', $session->get_timezone(), 'Y-m-d h:ia'), '', '', '');
+
+	 
+	echo $formwriter->datetimeinput('Key expires time (optional)', 'apk_expires_time', 'ctrlHolder', LibraryFunctions::convert_time($api_key->get('apk_expires_time'), 'UTC', $session->get_timezone(), 'Y-m-d h:ia'), '', '', '');
+	
 	echo $formwriter->start_buttons();
 	echo $formwriter->new_form_button('Submit');
 	echo $formwriter->end_buttons();
@@ -103,9 +117,9 @@
 
 
 
-	$api_keyt->end_box();
+	$page->end_box();
 	
 
-	$api_keyt->admin_footer();
+	$page->admin_footer();
 
 ?>
---
+++ public_html/adm/admin_api_keys.php
@@ -0,0 +1,112 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/ErrorHandler.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/FormWriterMaster.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/AdminPage-uikit3.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/SessionControl.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/LibraryFunctions.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/api_keys_class.php');
+
+	$session = SessionControl::get_instance();
+	$session->check_permission(5);
+	$session->set_return();
+
+	$numperpage = 30; 
+	$offset = LibraryFunctions::fetch_variable('offset', 0, 0, '');
+	$sort = LibraryFunctions::fetch_variable('sort', 'api_key_id', 0, '');
+	$sdirection = LibraryFunctions::fetch_variable('sdirection', 'DESC', 0, '');
+	$search_criteria = array();
+
+	//$searchterm = LibraryFunctions::fetch_variable('searchterm', '', 0, '');
+
+	//ONLY SHOW DELETED TO SUPER ADMINS
+	if($_SESSION['permission'] < 10){
+		$search_criteria['deleted'] = false;
+	}
+
+	$api_keys = new MultiApiKey(
+		$search_criteria,  //SEARCH CRITERIA
+		array($sort=>$sdirection),  //SORT AND DIRECTION array($usrsort=>$usrsdirection)
+		$numperpage,  //NUM PER PAGE
+		$offset,  //OFFSET
+		'OR');
+	$numrecords = $api_keys->count_all();
+	$api_keys->load();
+	
+
+	$page = new AdminPage();
+	$page->admin_header(	
+	array(
+		'menu-id'=> 'api_keys',
+		'page_title' => 'Add User',
+		'readable_title' => 'Add User',
+		'breadcrumbs' => array(
+			'ApiKeys'=>''
+		),
+		'session' => $session,
+	)
+	);
+
+
+
+	$headers = array("Name", "Public Key", 'Owner', "Start Time", "Expires Time", 'Status');
+	$altlinks = array();
+	$altlinks += array('Add ApiKey'=> '/admin/admin_api_key_edit');
+	$pager = new Pager(array('numrecords'=>$numrecords, 'numperpage'=> $numperpage));
+	$table_options = array(
+		//'sortoptions'=>array("User ID"=>"user_id", "Last Name"=>"last_name", "First Name"=>"first_name"),
+		'altlinks' => $altlinks,
+		'title' => 'ApiKeys',
+		//'search_on' => TRUE
+	);
+	$page->tableheader($headers, $table_options, $pager);	
+
+	foreach ($api_keys as $api_key){
+		$owner = new User($api_key->get('apk_usr_user_id'), TRUE);
+		$now = LibraryFunctions::get_current_time_obj('UTC');
+		$rowvalues = array();
+
+
+		array_push($rowvalues,  " <a href='/admin/admin_api_key?apk_api_key_id=$api_key->key'>".$api_key->get('apk_name')."</a>");
+		
+		array_push($rowvalues, $api_key->get('apk_public_key'));
+		array_push($rowvalues, $owner->display_name());
+
+		array_push($rowvalues, LibraryFunctions::convert_time($api_key->get('apk_start_time'), "UTC", $session->get_timezone(), 'M j, Y'));
+		array_push($rowvalues, LibraryFunctions::convert_time($api_key->get('apk_expires_time'), "UTC", $session->get_timezone(), 'M j, Y')); 		
+		
+		if($api_key->get('apk_delete_time')){
+			array_push($rowvalues, '<b>Deleted</b>');
+		}
+		else if(!$api_key->get('apk_is_active')){
+			array_push($rowvalues, '<b>Inactive</b>');
+		}
+		else if($api_key->get('apk_expires_time') && $api_key->get('apk_expires_time') < $now){
+			array_push($rowvalues, '<b>Expired</b>');
+		}
+		else if($api_key->get('apk_start_time') && $api_key->get('apk_start_time') > $now){
+			array_push($rowvalues, '<b>Scheduled</b>');
+		}
+		else{
+			array_push($rowvalues, '<b>Active</b>');
+		}
+/*		
+		else{
+			$delform = '<form id="form2" class="form2" name="form2" method="POST" action="/admin/admin_api_key_permanent_delete?apk_api_key_id='. $api_key->key.'">
+			<input type="hidden" class="hidden" name="action" value="removeapi_key" />
+			<input type="hidden" class="hidden" name="apk_api_key_id" value="'.$api_key->key.'" />
+			<button type="submit">Delete</button>
+			</form>';
+			array_push($rowvalues, $delform);
+		}
+*/
+
+		
+		$page->disprow($rowvalues);
+	}
+	$page->endtable($pager);
+
+	$page->admin_footer();
+?>
+
+
