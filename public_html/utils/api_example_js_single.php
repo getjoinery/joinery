ea74b456f6fd77d33cfaf7228c198ddf69b864fc
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-10-29 18:10:37 -0400

    Another api example.

---
+++ public_html/utils/api_example_js_single.php
@@ -0,0 +1,40 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<form id="contactForm1" action="/none" method="post">
+		<p id="results"></p>
+	</form>
+
+<script type="text/javascript">
+	// this is the id of the form
+	$( document ).ready(function() {
+
+		var form = $(this);
+		var actionUrl = 'https://jeremytunnell.net/api/v1/user/41';
+		var pub_key = 'public_fn4ini750e8pkjwq';
+		var secret_key = 'test1';
+		
+		$.ajax({
+			type: "GET",
+			headers: { 'public_key': pub_key, 'secret_key': secret_key },
+			url: actionUrl,
+			data: form.serialize(), // serializes the form's elements.
+			success: function(data)
+			{
+			  if(data['errortype'] == 'TransactionError'){
+				  $("#results").html(data['error']);
+				  console.log(data);
+			  }
+			  else if(data['errortype'] == 'AuthenticationError'){
+				  $("#results").html('There was an error.  Please contact the webmaster.');
+				  console.log(data);
+			  }
+			  else{
+				//console.log(data['data']);
+				$("#results").html(data['data']['usr_first_name']);
+			  }
+              
+			}
+		});
+		
+	}); 
+	</script>
