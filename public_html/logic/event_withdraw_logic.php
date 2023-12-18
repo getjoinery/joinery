82d567f2bea771aecd259b58ac52186e5a322555
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-12-18 08:26:33 -0500

    Bug fixes.

--- public_html/logic/event_withdraw_logic.php
+++ public_html/logic/event_withdraw_logic.php
@@ -25,14 +25,22 @@ if ($_POST){
 	
 	if ($confirm) {
 
-		$event_registrant = new EventRegistrant($evr_event_registrant_id, TRUE);
-		$event = new Event($event_registrant->get('evr_evt_event_id'),true);
-		$event_registrant->authenticate_write(array('current_user_id'=>$session->get_user_id(), 'current_user_permission'=>$session->get_permission()));
-		$event_registrant->remove();
+		if(EventRegistrant::check_if_exists($evr_event_registrant_id)){
+			$event_registrant = new EventRegistrant($evr_event_registrant_id, TRUE);
+			$event = new Event($event_registrant->get('evr_evt_event_id'),true);
+			$event_registrant->authenticate_write(array('current_user_id'=>$session->get_user_id(), 'current_user_permission'=>$session->get_permission()));
+			$event_registrant->remove();
+			
+			$msgtxt = 'You have now withdrawn from '.$event->get('evt_name').'.';
+			$message = new DisplayMessage($msgtxt, 'Success', '/\/profile\/account/', DisplayMessage::MESSAGE_ANNOUNCEMENT, DisplayMessage::MESSAGE_DISPLAY_IN_PAGE, 'userbox', TRUE);
+			$session->save_message($message);	
+		}
+		else{
+			$msgtxt = 'You are no longer registered for the event.';
+			$message = new DisplayMessage($msgtxt, 'Success', '/\/profile\/account/', DisplayMessage::MESSAGE_ANNOUNCEMENT, DisplayMessage::MESSAGE_DISPLAY_IN_PAGE, 'userbox', TRUE);
+			$session->save_message($message);				
+		}
 		
-		$msgtxt = 'You have now withdrawn from '.$event->get('evt_name').'.';
-		$message = new DisplayMessage($msgtxt, 'Success', '/\/profile\/account/', DisplayMessage::MESSAGE_ANNOUNCEMENT, DisplayMessage::MESSAGE_DISPLAY_IN_PAGE, 'userbox', TRUE);
-		$session->save_message($message);	
 
 		header("Location: /profile");
 		exit();		
@@ -43,9 +51,14 @@ if ($_POST){
 else{
 	$evr_event_registrant_id = LibraryFunctions::fetch_variable('evr_event_registrant_id', NULL, 1, 'You must provide a registrant.');
 
-	$event_registrant = new EventRegistrant($evr_event_registrant_id, true);
-	$user = new User($event_registrant->get('evr_usr_user_id'), TRUE);
-	$event = new Event($event_registrant->get('evr_evt_event_id'),true);
+	if(EventRegistrant::check_if_exists($evr_event_registrant_id)){
+		$event_registrant = new EventRegistrant($evr_event_registrant_id, true);
+		$user = new User($event_registrant->get('evr_usr_user_id'), TRUE);
+		$event = new Event($event_registrant->get('evr_evt_event_id'),true);
+	}
+	else{
+		$event_registrant = NULL;
+	}
 	
 }
 	
