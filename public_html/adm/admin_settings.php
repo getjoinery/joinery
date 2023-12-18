85771b89a6c575b47657144fa5c1852983635117
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2023-12-17 19:02:02 -0500

    Show system version

--- public_html/adm/admin_settings.php
+++ public_html/adm/admin_settings.php
@@ -96,6 +96,9 @@
 	echo $formwriter->begin_form('form', 'POST', '/admin/admin_settings');
 	
 	if($_SESSION['permission'] == 10){
+		echo 'System Version: '.$settings->get_setting('system_version').'<br>';
+		echo 'Database Version: '.$settings->get_setting('database_version').'<br>';
+		
 		echo '<b>NOTE: These settings will not override the settings if they are located in the Globalvars_site.php file in the /config directory</b><br>';
 		if($_SESSION['test_mode'] || $settings->get_setting('debug')){
 			echo '<div style="border: 3px solid red; padding: 10px; margin: 10px;">Test or debug mode is on.</div>';
