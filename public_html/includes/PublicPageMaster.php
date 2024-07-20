f14f7c8f80cb2f24b9f0f906961680fa279fb311
Jeremy Tunnell <jeremy.tunnell@gmail.com>
2024-07-20 08:59:37 -0400

    Cleanup of some settings and removing 'secure' and stray 'debug' settings.

--- public_html/includes/PublicPageMaster.php
+++ public_html/includes/PublicPageMaster.php
@@ -23,38 +23,17 @@ class PublicPageMaster {
 		'track' => TRUE,
 	);
 	
+	
+	//SECURE ARGUMENT HAS BEEN DEPRECATED
 	public function __construct($secure=FALSE) {
 		$this->rowcount = 0;
-		$this->secure = $secure;
 		$this->server = $_SERVER['PHP_SELF'];
 		$this->remote_addr = $_SERVER['REMOTE_ADDR'];
 
 		$settings = Globalvars::get_instance();
 		$session = SessionControl::get_instance();
-
-		$this->debug = $settings->get_setting('debug');
 		$this->debug_css = $settings->get_setting('debug_css');
-		if ($this->debug == 1) {
-			$secure = FALSE;
-			$this->secure = FALSE;
-		}
 
-		// If secure is on, they are not HTTPS and on port 80, forward them to SSL
-		/*
-		if ($secure && $_SERVER["SERVER_PORT"] == 80) {
-			header("HTTP/1.1 301 Moved Permanently");
-			header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
-			exit;
-		} else if (!$secure && $_SERVER["SERVER_PORT"] == 443) {
-			// Likewise if they aren't secure and reading an SSLed page, redirect them to non-SSL
-			header("HTTP/1.1 301 Moved Permanently");
-			header("Location: http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
-			exit;
-		}
-		*/
-
-		$this->protocol = $this->secure ? 'https://' : 'http://';
-		$this->secure_prefix = ($this->debug == 0) ? $settings->get_setting('webDir') : $settings->get_setting('webDir');
 
 		
 		$this->location_data = $session->get_location_data();
@@ -93,12 +72,6 @@ class PublicPageMaster {
 		header('X-Frame-Options: SAMEORIGIN');
 		header('X-Content-Type-Options: nosniff');
 		header('Referrer-Policy: unsafe-url');
-
-		$this->debug = $settings->get_setting('debug');
-		if ($this->debug == 1) {
-			$secure = FALSE;
-			$this->secure = FALSE;
-		}
 		
 	}
 	
