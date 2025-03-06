736a624f024b3a1dde81f64a419899bedfb72bf8
getjoinery <jeremy@jeremytunnell.com>
2025-03-06 12:37:59 -0500

    Subscription refactor.

--- public_html/logic/orders_recurring_action_logic.php
+++ public_html/logic/orders_recurring_action_logic.php
@@ -20,47 +20,7 @@
 	
 	$order_item_id = LibraryFunctions::fetch_variable('order_item_id', NULL,1,'order_item_id');
 	$order_item = new OrderItem($order_item_id, TRUE);	
-	$order_user = new User($order_item->get('odi_usr_user_id'), TRUE);
-	$order = $order_item->get_order();
-	$order_item->authenticate_write(array('current_user_id'=>$session->get_user_id(), 'current_user_permission'=>$session->get_permission()));
-
-	$stripe_subscription = $stripe_helper->update_subscription_in_order_item($order_item);
-	if(!$stripe_subscription){
-		throw new SystemDisplayablePermanentError("We were unable to retrieve that subscription (".$order_item->get('odi_stripe_subscription_id').") Please contact the webmaster.");
-		exit;		
-	}
-
-	if(!$order_item->get('odi_subscription_cancelled_time')){
-				
-		$stripe_subscription = $stripe_helper->cancel_subscription($order_item->get('odi_stripe_subscription_id'), 'immediate');
-		if(!$stripe_subscription){
-			throw new SystemDisplayablePermanentError("We were unable to cancel that subscription (".$order_item->get('odi_stripe_subscription_id').") Please contact the webmaster.");
-			exit;		
-		}			
-		$result = $stripe_helper->update_subscription_in_order_item($order_item);
-		
-		//SEND NOTIFICATION
-		if($settings->get_setting('subscription_notification_emails')){
-			$notify_emails = explode(',', $settings->get_setting('subscription_notification_emails'));
-			foreach($notify_emails as $notify_email){
-				try {
-					$notify_user = User::GetByEmail($notify_email);
-					$body = 'Subscription '.$order_item->get('odi_stripe_subscription_id').' (Order '. $order->key .') was cancelled for user '.$order_user->display_name().' ('.$order_user->get('usr_email').')';
-					$email_inner_template = $settings->get_setting('individual_email_inner_template');
-					$email = new EmailTemplate($email_inner_template, $notify_user);
-					$email->fill_template(array(
-						'subject' => 'Cancelled Subscription',
-						'body' => $body,
-					));	
-					$result = $email->send();
-				}					
-				catch (Exception $e) {
-					//DO NOTHING
-					$error2 = "";
-				}
-			}
-		}
-	}
+	$success = $order_item->cancel_subscription_order_item(true);
 
 
 	//NOW REDIRECT
