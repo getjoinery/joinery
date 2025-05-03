761a3f99ba69ced6f719ea01587c6d3d46d577bd
getjoinery <jeremy@jeremytunnell.com>
2025-05-03 15:34:07 -0400

    Remove default theme.  From now on, there is just base files and optional chosen theme.

--- public_html/views/blog.php
+++ public_html/views/blog.php
@@ -90,40 +90,6 @@
 			<?php } ?>
 			</section>
 
-<?php /*
-<div>
-  <div class="sm:hidden">
-    <label for="tabs" class="sr-only">Select a tab</label>
-    <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-    <select id="tabs" name="tabs" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-      <option <?php if(!$_REQUEST['tab']){ echo 'selected'; } ?>>Active Events</option>
-
-      <option <?php if($_REQUEST['tab'] == 'past'){ echo 'selected'; } ?>>Past Events</option>
-
-    </select>
-  </div>
-  <div class="hidden sm:block">
-    <div class="border-b border-gray-200">
-      <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-        <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
-		<?php
-		$current_style = 'class="border-indigo-500 text-indigo-600 group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm" aria-current="page"';
-		$standard_style = 'class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"';
-		?>
-        <a href="/profile/profile"  <?php if(!$_REQUEST['tab']){ echo $current_style; } else{ echo $standard_style; } ?>>
-          Active Events
-        </a>
-
-        <a href="/profile/profile?tab=past" <?php if($_REQUEST['tab'] == 'past'){ echo $current_style; } else{ echo $standard_style; } ?>>
-          Past Events
-        </a>
-      </nav>
-    </div>
-  </div>
-</div>
-*/
-?>
-
 
         </div>
 
@@ -197,72 +163,7 @@
 		  
 		<?php } ?>			
 			
-          <!-- Order History -->
-		  <?php
-		  /*
-			if($settings->get_setting('products_active')){
-				?>
-				<section aria-labelledby="recent-hires-title">
-					<div class="rounded-lg bg-white overflow-hidden shadow">
-					  <div class="p-6">
-						<h2 class="text-base font-medium text-gray-900" id="recent-hires-title">Your Orders</h2>
-						<div class="flow-root mt-6">
-						  <ul role="list" class="-my-5 divide-y divide-gray-200">
-						  <?php
-				
-				
-				foreach($orders as $order) {
-					?>
-					<li class="py-4">
-					  <div class="flex items-center space-x-4">
-					  <!--
-						<div class="flex-shrink-0">
-						  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-						</div>
-						-->
-						<div class="flex-1 min-w-0">
-						  <p class="text-sm font-medium text-gray-900 truncate">
-							Order <?php echo $order->key. ' ($'.$order->get('ord_total_cost').')'; ?>
-						  </p>
-						  
-						  <p class="text-sm text-gray-500 truncate">
-							<?php echo  LibraryFunctions::convert_time($order->get('ord_timestamp'), 'UTC', $session->get_timezone(), 'M d, Y'); ?>
-						  </p>
-						  
-						</div>
-						<?php
-						
-						if($action){
-						?>
-						<div>
-						  <?php echo $action; ?>
-						</div>
-						<?php
-						}
-						
-						?>
-					  </div>
-					</li>
-				<?php
-				}
-				
-				?>
-				  
-                  </ul>
-                </div>
-                <div class="mt-6">
-					<a href="/product/recurring-donation" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
-						See All Orders
-					  </a>
-                </div>
-              </div>
-            </div>
-          </section>
-		  <?php
-		  
-			}
-			*/
-			?>			
+	
 			
         </div>
 		
