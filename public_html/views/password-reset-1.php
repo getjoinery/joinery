a86096c0a0d625c81ce81cdd8b348fcd1e18fb23
getjoinery <jeremy@jeremytunnell.com>
2025-05-04 15:13:56 -0400

    Reformatting to falcon theme.

--- public_html/views/password-reset-1.php
+++ public_html/views/password-reset-1.php
@@ -4,31 +4,94 @@
 	require_once (LibraryFunctions::get_logic_file_path('password-reset-1_logic.php'));
 
 	$page_vars = password_reset_1_logic($_GET, $_POST);
+	$settings = Globalvars::get_instance();
 
 	$page = new PublicPage();
 	$hoptions=array(
 		'is_valid_page' => $is_valid_page,
 		'title'=>'Password Reset', 
+		'header_only' => true,
 	);	
 	$page->public_header($hoptions,NULL);
-	echo PublicPage::BeginPage('Reset Password - Step 1 of 2');
-	echo PublicPage::BeginPanel();
+	//echo PublicPage::BeginPage('Reset Password - Step 1 of 2');
+	//echo PublicPage::BeginPanel();
 
 	if($page_vars['message']){
-		echo PublicPage::alert($page_vars['message_title'], $page_vars['message'], $page_vars['message_type']);
+		?>
+		<main class="main" id="top">
+		  <div class="container" data-layout="container">
+
+			<div class="row flex-center min-vh-100 py-6">
+			  <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+				<a class="d-flex flex-center mb-4" href="/">
+				<?php
+		  		if($settings->get_setting('logo_link')){
+					echo '<img class="me-2" src="'.$settings->get_setting('logo_link').'" alt="" width="40" />';
+				}
+				?>
+				<span class="font-sans-serif text-primary fw-bolder fs-4 d-inline-block"><?php echo $settings->get_setting('site_name'); ?></span>
+				</a>
+				<div class="card">
+				  <div class="card-body p-4 p-sm-5">
+					<div class="text-center"><!--<img class="d-block mx-auto mb-4" src="../../../assets/img/icons/spot-illustrations/16.png" alt="Email" width="100" />-->
+					  <h4 class="mb-2">Please check your email!</h4>
+					  <p>An email has been sent to you. Please click on the included link to reset <span class="white-space-nowrap">your password.</span>
+					  </p><a class="btn btn-primary btn-sm mt-3" href="/login"><span class="fas fa-chevron-left me-1" data-fa-transform="shrink-4 down-1"></span>Return to login</a>
+					</div>
+				  </div>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</main>		
+		<?php
 	}
 	else{
-		$settings = Globalvars::get_instance();
 		$formwriter = LibraryFunctions::get_formwriter_object('form1', $settings->get_setting('form_style'));
-		echo $formwriter->begin_form("", "post", "/password-reset-1", true); 
-		echo $formwriter->textinput("Enter the Email Address you registered with", "email", NULL, 20, htmlspecialchars($email), '', 64, NULL);
-		echo $formwriter->new_form_button('Submit');
-		echo $formwriter->end_form();
+		
+		?>
+		<main class="main" id="top">
+		  <div class="container" data-layout="container">
+			<div class="row flex-center min-vh-100 py-6 text-center">
+			  <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+				<a class="d-flex flex-center mb-4" href="/">
+				<?php
+		  		if($settings->get_setting('logo_link')){
+					echo '<img class="me-2" src="'.$settings->get_setting('logo_link').'" alt="" width="40" />';
+				}
+				?>
+				<span class="font-sans-serif text-primary fw-bolder fs-4 d-inline-block"><?php echo $settings->get_setting('site_name'); ?></span>
+				</a>
+				<div class="card">
+				  <div class="card-body p-4 p-sm-5">
+					<h5 class="mb-0">Forgot your password?</h5><small>Enter your email and we'll send you a reset link.</small>
+					<div class="mt-4">
+						<?php 
+						$validation_rules = array();
+						$validation_rules['email']['required']['value'] = 'true';
+						$validation_rules['password']['required']['value'] = 'true';
+						echo $formwriter->set_validate($validation_rules);	
+						echo $formwriter->begin_form("", "post", "/password-reset-1", true);
+
+						?>
+					  <input class="form-control" type="email" name="email" id="email" placeholder="Email address" />
+					  <div class="mb-3"></div>
+					  <button class="btn btn-primary d-block w-100 mt-3" type="submit" name="submit">Send reset link</button>
+					  <?php echo $formwriter->end_form(); ?>
+					</div><a class="fs-10 text-600" href="/login">Back to login</a>
+				  </div>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</main>		
+		<?php
+		
 	}
 
-	echo PublicPage::EndPanel();
-	echo PublicPage::EndPage();
-	$page->public_footer($foptions=array('track'=>TRUE));
+	//echo PublicPage::EndPanel();
+	//echo PublicPage::EndPage();
+	$page->public_footer($foptions=array('track'=>TRUE, 'header_only' => true));
 
 
 ?>
