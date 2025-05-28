acbf6c221a7af1a38e76d37723060ac42eef7ed4
getjoinery <jeremy@jeremytunnell.com>
2025-05-28 13:57:57 -0400

    Bug fix.

--- theme/sassa/includes/PublicPage.php
+++ theme/sassa/includes/PublicPage.php
@@ -263,7 +263,7 @@ Career Area
 
 
     <!-- Jquery -->
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script> 
 
     <!--==============================
 	    All CSS File
