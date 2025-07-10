c8018ad099d81e7613124f76c075b5327ebee819
getjoinery <jeremy@jeremytunnell.com>
2025-07-10 08:25:06 -0400

    Stop tracking theme and plugin files - now using symlinks to separate repositories

--- public_html/theme/tailwind/views/robots.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/data/settings_class.php');
-	$settings = Globalvars::get_instance();
-	
-header('Content-Type: text/plain');
-echo $settings->get_setting('robots_text');
-
-
-?>
