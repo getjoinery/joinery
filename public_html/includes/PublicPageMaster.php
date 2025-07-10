2ffcfd52cc1b9bcba528bd05af1f45e24fea118c
getjoinery <jeremy@jeremytunnell.com>
2025-07-09 21:34:28 -0400

    Cleaned up force_https

--- public_html/includes/PublicPageMaster.php
+++ public_html/includes/PublicPageMaster.php
@@ -48,16 +48,10 @@ class PublicPageMaster {
 		
 		//https://blog.vnaik.com/posts/web-attacks.html
 		if($settings->get_setting('force_https')){
-			$isSecure = false;
-			if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-				$isSecure = true;
-			}
-			elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
-				$isSecure = true;
-			}
-			$REQUEST_PROTOCOL = $isSecure ? 'https' : 'http';
-			if($REQUEST_PROTOCOL == 'http'){
-				 $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			// Use the more robust isSecure() method from LibraryFunctions
+			require_once('LibraryFunctions.php');
+			if(!LibraryFunctions::isSecure()){
+				$location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 				header('HTTP/1.1 301 Moved Permanently');
 				header('Location: ' . $location);
 				exit;
