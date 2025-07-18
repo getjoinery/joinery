83fa084c8e9c7922c967726601b7ebc8f65bf00d
getjoinery <jeremy@jeremytunnell.com>
2025-07-18 10:23:52 -0400

    Remove old jquery upload code.

--- public_html/includes/AdminPage-uikit3.php
+++ public_html/includes/AdminPage-uikit3.php
@@ -84,15 +84,7 @@ class AdminPage{
 			<!-- blueimp Gallery styles for uploader-->
 			<link rel="stylesheet" href="/includes/jquery-file-upload/css/blueimp-gallery.min.css"/>
 			<!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
-			<link rel="stylesheet" href="/includes/jquery-file-upload/css/jquery.fileupload.css" />
-			<link rel="stylesheet" href="/includes/jquery-file-upload/css/jquery.fileupload-ui.css" />
-			<!-- CSS adjustments for browsers with JavaScript disabled -->
-			<noscript
-			  ><link rel="stylesheet" href="/includes/jquery-file-upload/css/jquery.fileupload-noscript.css"
-			/></noscript>
-			<noscript
-			  ><link rel="stylesheet" href="/includes/jquery-file-upload/css/jquery.fileupload-ui-noscript.css"
-			/></noscript>
+
 			<!--end Gallery styles-->	
 
 			<?php
