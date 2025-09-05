bdd2c4248f817079b515536ed3a4f9f2a60f8424
getjoinery <jeremy@jeremytunnell.com>
2025-09-05 11:23:52 +0000

    Tables refactor

--- public_html/includes/PublicPageBase.php
+++ public_html/includes/PublicPageBase.php
@@ -10,7 +10,7 @@ PathHelper::requireOnce('includes/PluginHelper.php');
 PathHelper::requireOnce('data/users_class.php');
 PathHelper::requireOnce('data/public_menus_class.php');
 
-class PublicPageBase {
+abstract class PublicPageBase {
 
 	protected $rowcount;
 
@@ -331,36 +331,241 @@ class PublicPageBase {
 	}
 
 
-	function tableheader($headers, $class='table cart-table', $id='table1'){
-		echo '<table class="'.$class.'" id="'.$id.'" cellspacing="0">
-			<thead><tr>';
-
-		foreach ($headers as $value) {
-			printf('<th scope="col" abbr="%s">%s</th>', $value, $value);
+	function tableheader($headers, $options=array(), $pager=NULL){
+		$this->begin_box($options);
+		
+		if(!$pager){
+			$pager = new Pager();
 		}
-		echo '</tr></thead><tbody>';
+
+		$sortoptions = null;
+		if(isset($options['sortoptions'])){
+			$sortoptions = $options['sortoptions'];
+		}
+		
+		$filteroptions = null;
+		if(isset($options['filteroptions'])){
+			$filteroptions = $options['filteroptions'];
+		}
+		
+		$search_on = null;
+		if(isset($options['search_on'])){
+			$search_on = $options['search_on'];
+		}
+
+		// Get theme-specific CSS classes
+		$css = $this->getTableClasses();
+
+		echo '<div class="row justify-content-end justify-content-end gx-3 gy-0 px-3">';
+
+		if($sortoptions){
+			echo '<div class="col-sm-auto">';
+			printf('<form method="get" ACTION="%s">', $pager->base_url());
+			echo $pager->url_vars_as_hidden_input(array('sort', 'sdirection'));
+			echo '<label for="'.$pager->prefix().'sort'.'">Sort: </label><select name="'.$pager->prefix().'sort'.'">';
+			foreach ($sortoptions as $key => $value) {
+				if($pager->get_sort() == $value){
+					echo "<option value='$value' selected=selected>$key";
+				}
+				else{
+					echo "<option value='$value'>$key";
+				}
+			}
+			echo '</select>';
+			
+
+			echo '<label for="'.$pager->prefix().'sdirection'.'"> </label><select name="'.$pager->prefix().'sdirection'.'">';
+			$diroptions = array('Descending'=>'DESC', 'Ascending'=>'ASC');
+			foreach ($diroptions as $key => $value) {
+				if($pager->sort_direction() == $value){
+					echo "<option value='$value' selected=selected>$key";
+				}
+				else{
+					echo "<option value='$value'>$key";
+				}
+			}
+			echo '</select>';
+
+						
+			foreach($pager->url_vars() as $key=>$value){
+				echo '<input type="hidden" name="'.$key.'" value="'.$value.'">';
+			}
+
+			echo '<input type="submit" value="sort" /></form>'; 
+			echo '</div>';
+		}
+
+		if($filteroptions){
+			echo '<div class="col-sm-auto">';
+			printf('<form method="get" ACTION="%s">', $pager->base_url());
+			echo $pager->url_vars_as_hidden_input(array('filter'));
+			echo '<label for="'.$pager->prefix().'filter'.'">Show: </label><select name="'.$pager->prefix().'filter'.'">';
+			foreach ($filteroptions as $key => $value) {
+				if($pager->get_filter() == $value){
+					echo "<option value='$value' selected=selected>$key";
+				}
+				else{
+					echo "<option value='$value'>$key";
+				}
+			}
+			echo '</select>';
+
+						
+			foreach($pager->url_vars() as $key=>$value){
+				echo '<input type="hidden" name="'.$key.'" value="'.$value.'">';
+			}
+
+			echo '<input type="submit" value="submit" /></form>'; 
+			echo '</div>';
+		}
+
+		if($search_on){
+			echo '<div class="col-sm-auto">';
+			PathHelper::requireOnce('includes/LibraryFunctions.php');
+			$formwriter = LibraryFunctions::get_formwriter_object('search_form', 'admin');
+
+			echo $formwriter->begin_form("search_form", "get", $pager->base_url());
+			echo $pager->url_vars_as_hidden_input(array('searchterm'));
+			echo '<label for="searchterm">Search: </label>
+						  <input name="'.$pager->prefix().'searchterm" id="'.$pager->prefix().'searchterm" value="'.$pager->search_term().'" size="20" type="text" class="textInput" maxlength="">';	
+			
+			foreach($pager->url_vars() as $key=>$value){
+				echo '<input type="hidden" name="'.$key.'" value="'.$value.'">';
+			}	
+
+			echo '<input type="submit" value="Search" />';
+			echo $formwriter->end_form();
+			echo '</div>';
+		}
+
+		echo '</div>';
+
+		// Use theme-specific wrapper and table classes
+		$wrapperClass = isset($css['wrapper']) ? $css['wrapper'] : 'table-responsive';
+		$tableClass = isset($css['table']) ? $css['table'] : 'table';
+		
+		echo '<div class="'.$wrapperClass.'">
+	  <table class="'.$tableClass.'">
+			<tr>';
+
+			foreach ($headers as $value) {
+				echo '<th>'.$value.'</th>';
+			}
+
+		echo '</tr>';
 	}
 
 	function disprow($dataarray){
-
 		echo '<tr>';
 
 		foreach ($dataarray as $value) {
 			if ($value == "") {
 				$value = "&nbsp";
 			}
-
-
 			printf('<td>%s</td>', $value);
-
 		}
 		echo "</tr>\n";
-		$this->rowcount++;
 	}
 
-	function endtable(){
-		echo '</tbody></table>';
+	function endtable($pager=NULL){
+		if(!$pager){
+			$pager = new Pager();
+		}
+		echo '</table></div>';
+
+		
+		//PAGE
+		if($pager->num_records()){	
+			echo '<div class="d-flex align-items-center justify-content-center position-relative mt-3">';
+						echo '<div class="position-absolute start-0 mb-0 fs-10"> '.$pager->num_records().' records, Page '.$pager->current_page() .' of '.$pager->total_pages().'</div>';
+
+						echo '<div><div class="d-flex justify-content-center mt-3">';
+		
+						if($pager->num_records() > $pager->num_per_page()){
+							if($page_number = $pager->is_valid_page('-10')){
+								echo '<a href="'.$pager->get_url($page_number).'"><button class="btn btn-sm btn-falcon-default me-1" type="button" title="Previous 10" data-list-pagination="prev"><svg class="svg-inline--fa fa-chevron-left fa-w-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path></svg><!-- <span class="fas fa-chevron-left"></span> Font Awesome fontawesome.com --></button></a>';
+							}
+							else{
+								echo '<button class="btn btn-sm btn-falcon-default me-1 disabled" type="button" title="Previous 10" data-list-pagination="prev" disabled=""><svg class="svg-inline--fa fa-chevron-left fa-w-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path></svg><!-- <span class="fas fa-chevron-left"></span> Font Awesome fontawesome.com --></button>';
+							}
+											
+							echo '<ul class="pagination mb-0">';
+							for($x=4; $x>=1;$x--){
+								if($page_number = $pager->is_valid_page('-'.$x)){
+									echo '<a href="'.$pager->get_url($page_number).'"><button class="page btn btn-sm btn-falcon-default" type="button" data-i="2" data-page="5">'.$page_number.'</button></a> ';
+								}
+							}
+							
+							echo '<li class="active"><button class="page btn btn-sm btn-falcon-default disabled" type="button" disabled="">'.$pager->current_page().'</button></li> ';
+							
+							for($x=1; $x<=4;$x++){
+								if($page_number = $pager->is_valid_page('+'.$x)){
+									echo '<a href="'.$pager->get_url($page_number).'"><button class="page btn btn-sm btn-falcon-default" type="button" data-i="2" data-page="5">'.$page_number.'</button></a> ';
+								}
+							}	
+							echo '</ul>';
+						
+							if($page_number = $pager->is_valid_page('+10')){
+								echo '<a href="'.$pager->get_url($page_number).'"><button class="btn btn-sm btn-falcon-default ms-1" type="button" title="Next 10" data-list-pagination="next"><svg class="svg-inline--fa fa-chevron-right fa-w-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <span class="fas fa-chevron-right"></span> Font Awesome fontawesome.com --></button></a>';
+							}
+							else{
+								echo '<button class="btn btn-sm btn-falcon-default ms-1 disabled" type="button" title="Next 10" data-list-pagination="next" disabled=""><svg class="svg-inline--fa fa-chevron-right fa-w-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <span class="fas fa-chevron-right"></span> Font Awesome fontawesome.com --></button>';
+							}
+						}
+			
+			
+			echo '</div></div>';
+			echo '</div>';
+		
+		}
+		
+		$this->end_box();
 	}
+
+	function begin_box($options=NULL){
+		echo '<div>';
+		$this->dropdown_or_buttons($options);
+	}
+	
+	function end_box(){
+		echo '</div>';
+	}
+
+	function dropdown_or_buttons($options=array()){
+		if(!is_array($options)){
+			$options = array();
+		}
+
+		if(!isset($options['options_label'])){
+			$options['options_label'] = 'Options';
+		}		
+
+		if(isset($options['altlinks']) && is_array($options['altlinks'])){
+			echo '<div class="row justify-content-end justify-content-end gx-3 gy-0 px-3"><div class="col-sm-auto">';
+			if(count($options['altlinks']) > 2){
+				echo '<div class="dropdown font-sans-serif d-inline-block mb-2"><button class="btn btn-falcon-default dropdown-toggle" id="dropdownMenuButton" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'.$options['options_label'].'</button><div class="dropdown-menu dropdown-menu-end py-0" aria-labelledby="dropdownMenuButton">';
+				foreach($options['altlinks'] as $label=>$link){
+					echo '<a href="'.$link.'" class="dropdown-item">'.$label.'</a></li>';
+				}	
+				echo '</div></div>';	    
+										
+			}
+			else if(count($options['altlinks']) > 0){
+				
+				foreach($options['altlinks'] as $label=>$link){
+					echo '<a href="'.$link.'"><button class="btn btn-outline-secondary me-1 mb-1" type="button">'.$label.'</button></a>';
+				}
+				
+			}
+			echo '</div></div>';
+		}			
+	}
+
+	/**
+	 * Get theme-specific CSS classes for table styling
+	 * @return array Array of CSS class mappings
+	 */
+	abstract protected function getTableClasses();
 
 	/**
 	 * Check if admin bar should be displayed
