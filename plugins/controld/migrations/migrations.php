3a9d6fbb405a6231e1a8ce5ac94aec6e95422b48
getjoinery <jeremy@jeremytunnell.com>
2025-10-04 13:30:48 +0000

    Remove unused files and migrations

--- plugins/controld/migrations/migrations.php
+++ plugins/controld/migrations/migrations.php
@@ -45,40 +45,6 @@ return [
             
             return true;
         }
-    ],
-    [
-        'id' => '002_controld_admin_menu',
-        'version' => '1.0.0',
-        'depends_on' => ['001_controld_initial_setup'],
-        'description' => 'Add ControlD admin menu entry',
-        'up' => function($dbconnector) {
-            $dblink = $dbconnector->get_db_link();
-            
-            // Add admin menu entry (converted from migration 20250201)
-            $check_sql = "SELECT count(1) as count FROM amu_admin_menus WHERE amu_defaultpage = '/plugins/controld/admin/admin_ctld_accounts'";
-            $check_q = $dblink->prepare($check_sql);
-            $check_q->execute();
-            $result = $check_q->fetch(PDO::FETCH_ASSOC);
-            
-            if ($result['count'] == 0) {
-                $sql = "INSERT INTO amu_admin_menus (amu_menudisplay, amu_parent_menu_id, amu_defaultpage, amu_order, amu_min_permission, amu_disable, amu_icon, amu_slug, amu_setting_activate) 
-                        VALUES ('Accounts', NULL, '/plugins/controld/admin/admin_ctld_accounts', 5, 8, 0, '', 'accounts', 'controld_key')";
-                $q = $dblink->prepare($sql);
-                $q->execute();
-            }
-            
-            return true;
-        },
-        'down' => function($dbconnector) {
-            $dblink = $dbconnector->get_db_link();
-            
-            // Remove admin menu entry
-            $sql = "DELETE FROM amu_admin_menus WHERE amu_defaultpage = '/plugins/controld/admin/admin_ctld_accounts'";
-            $q = $dblink->prepare($sql);
-            $q->execute();
-            
-            return true;
-        }
     ]
 ];
 ?>
