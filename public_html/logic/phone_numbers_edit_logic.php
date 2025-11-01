b483b9adc782b6ef7870938c70326be10348579f
getjoinery <jeremy@jeremytunnell.com>
2025-11-01 16:32:10 +0000

    More v2 conversions

--- public_html/logic/phone_numbers_edit_logic.php
+++ public_html/logic/phone_numbers_edit_logic.php
@@ -2,55 +2,68 @@
 require_once(__DIR__ . '/../includes/PathHelper.php');
 
 function phone_numbers_edit_logic($get_vars, $post_vars){
-	require_once(PathHelper::getIncludePath('includes/Activation.php'));
-require_once(PathHelper::getIncludePath('includes/LogicResult.php'));
-
-	require_once(PathHelper::getIncludePath('includes/LibraryFunctions.php'));
-	require_once(PathHelper::getIncludePath('includes/SessionControl.php'));
-
+	require_once(PathHelper::getIncludePath('includes/LogicResult.php'));
 	require_once(PathHelper::getIncludePath('data/phone_number_class.php'));
 
 	$session = SessionControl::get_instance();
 	$session->check_permission(0);
 
-	$phn_phone_number_id = LibraryFunctions::fetch_variable('phn_phone_number_id', NULL, 0, '');
+	$user_id = $session->get_user_id();
+
+	// Load or create phone number
+	if (isset($post_vars['edit_primary_key_value'])) {
+		$phone_number = new PhoneNumber($post_vars['edit_primary_key_value'], TRUE);
+		// Verify user owns this phone number
+		$phone_number->authenticate_write(array(
+			'current_user_id' => $user_id,
+			'current_user_permission' => $session->get_permission()
+		));
+	} elseif (isset($get_vars['phn_phone_number_id'])) {
+		$phone_number = new PhoneNumber($get_vars['phn_phone_number_id'], TRUE);
+		// Verify user owns this phone number
+		$phone_number->authenticate_write(array(
+			'current_user_id' => $user_id,
+			'current_user_permission' => $session->get_permission()
+		));
+	} else {
+		// Load user's first phone number or create new
+		$phone_numbers = new MultiPhoneNumber(array('user_id' => $user_id));
+		if ($phone_numbers->count_all()) {
+			$phone_numbers->load();
+			$phone_number = $phone_numbers->get(0);
+		} else {
+			$phone_number = new PhoneNumber(NULL);
+		}
+	}
 
 	if($post_vars){
-
-		if($post_vars['phn_phone_number_id']){
-			$phone_number = new PhoneNumber($post_vars['phn_phone_number_id'], TRUE);
-			$phone_number->authenticate_write(array('current_user_id'=>$session->get_user_id(), 'current_user_permission'=>$session->get_permission()));
-		}
-		else{
-			$phone_number = NULL;
+		// Add-only logic - set user_id when creating new phone number
+		if (!$phone_number->key) {
+			$phone_number->set('phn_usr_user_id', $user_id);
 		}
 
-		$phone_number = PhoneNumber::CreateFromForm($post_vars, $session->get_user_id(), $phone_number, FALSE);
-
-		if($phone_number){
-			$msgtxt = 'Addresses have been edited.';
-			$message = new DisplayMessage($msgtxt, 'Success', '/\/profile\/phone_numbers_edit.*/', DisplayMessage::MESSAGE_ANNOUNCEMENT, DisplayMessage::MESSAGE_DISPLAY_IN_PAGE, "phonebox", TRUE);
-			$session->save_message($message);
+		// Set editable fields
+		$editable_fields = array('phn_cco_country_code_id', 'phn_phone_number');
+		foreach($editable_fields as $field) {
+			if(isset($post_vars[$field])){
+				$phone_number->set($field, $post_vars[$field]);
+			}
 		}
+
+		$phone_number->prepare();
+		$phone_number->save();
+
+		// Success message
+		$msgtxt = 'Phone number has been saved.';
+		$message = new DisplayMessage($msgtxt, 'Success', '/\/profile\/phone_numbers_edit.*/',
+			DisplayMessage::MESSAGE_ANNOUNCEMENT, DisplayMessage::MESSAGE_DISPLAY_IN_PAGE, "phonebox", TRUE);
+		$session->save_message($message);
+
+		return LogicResult::redirect('/profile/phone_numbers_edit');
 	}
 
-	$user = new User($session->get_user_id(), TRUE);
-	$phone_numbers = new MultiPhoneNumber(
-		array('user_id'=>$user->key),
-		NULL,
-		30,
-		0);
-	$numphone = $phone_numbers->count_all();
-	$phone_numbers->load();
-	if($numphone){
-		$page_vars['phone_number'] = $phone_numbers->get(0);
-	}
-	else{
-		$page_vars['phone_number'] = new PhoneNumber(NULL);
-	}
-
+	$page_vars['phone_number'] = $phone_number;
 	$page_vars['display_messages'] = $session->get_messages($_SERVER['REQUEST_URI']);
-
 	$page_vars['tab_menus'] = array(
 		'Edit Account' => '/profile/account_edit',
 		'Change Password' => '/profile/password_edit',
@@ -58,8 +71,6 @@ require_once(PathHelper::getIncludePath('includes/LogicResult.php'));
 		'Edit Phone Number' => '/profile/phone_numbers_edit',
 		'Change Contact Preferences' => '/profile/contact_preferences',
 	);
-
-	$page_vars['display_messages'] = $session->get_messages($_SERVER['REQUEST_URI']);
 
 	return LogicResult::render($page_vars);
 }
