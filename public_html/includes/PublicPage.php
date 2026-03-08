fbcc4cb9d4d808cc9ed0d8a31cfd06195c4d218a
getjoinery <jeremy@jeremytunnell.com>
2026-03-08 20:54:53 +0000

    Always load base stylesheet before theme stylesheet

--- public_html/includes/PublicPage.php
+++ public_html/includes/PublicPage.php
@@ -89,7 +89,6 @@ class PublicPage extends PublicPageBase {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,700;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/custom.css">
 </head>
 <body>
