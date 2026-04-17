5bdf655c13e1210d3843b34028ff00b56b9f589d
getjoinery <jeremy@jeremytunnell.com>
2026-04-17 12:11:13 +0000

    Delete deprecated themes

--- public_html/theme/empoweredhealth/includes/FormWriter.php
+++
@@ -1,9 +0,0 @@
-<?php
-// FormWriter for Empowered Health theme - extends Bootstrap form writer
-require_once(PathHelper::getIncludePath('includes/FormWriterV2Bootstrap.php'));
-
-class FormWriter extends FormWriterV2Bootstrap {
-    // Inherits all form styling from FormWriterV2Bootstrap base class
-    // Empowered Health theme uses Bootstrap 5, so default Bootstrap styling applies
-}
-?>
--- public_html/theme/zoukphilly/includes/PublicPage.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(PathHelper::getIncludePath('includes/PublicPageTailwind.php'));
-
-class PublicPage extends PublicPageTailwind {
-    // Inherits full Tailwind PublicPage functionality
-    // Can override methods here if needed for theme-specific customizations
-}
-?>
