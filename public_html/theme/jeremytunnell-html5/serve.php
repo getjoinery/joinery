14a6182a66ff9e523f593aaec168ff6f6d62ffb6
getjoinery <jeremy@jeremytunnell.com>
2026-05-22 18:12:31 +0000

    Route bug fix

--- public_html/theme/jeremytunnell-html5/serve.php
+++ public_html/theme/jeremytunnell-html5/serve.php
@@ -2,9 +2,6 @@
 // theme/jeremytunnell-html5/serve.php - RouteHelper format routes
 
 $routes = [
-    'dynamic' => [
-        '/post/{slug}' => ['model' => 'Post', 'model_file' => 'data/posts_class'],
-    ],
-
+    'dynamic' => [],
     'custom' => [],
 ];
